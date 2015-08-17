7b1a0ec1a893e4cd5950c3f343513ecd1334012c
Arun Agrawal <arunagw@gmail.com>
2015-08-17 10:56:28 +0200

    Update repo on each build so we can track if repo becomes private

--- spec/jobs/buildable_spec.rb
+++ spec/jobs/buildable_spec.rb
@@ -1,9 +1,4 @@
-require "spec_helper"
-require "active_job"
-require "app/jobs/buildable"
-require "app/models/payload"
-require "app/services/build_runner"
-require "raven"
+require "rails_helper"
 
 describe Buildable do
   class BuildableTestJob < ActiveJob::Base
@@ -13,7 +8,8 @@ describe Buildable do
   describe "perform" do
     it 'runs build runner' do
       build_runner = double(:build_runner, run: nil)
-      payload = double("Payload")
+      payload_data = "some data"
+      payload = double("Payload", github_repo_id: 1)
       allow(Payload).to receive(:new).with(payload_data).and_return(payload)
       allow(BuildRunner).to receive(:new).and_return(build_runner)
 
@@ -22,6 +18,19 @@ describe Buildable do
       expect(Payload).to have_received(:new).with(payload_data)
       expect(BuildRunner).to have_received(:new).with(payload)
       expect(build_runner).to have_received(:run)
+    end
+
+    it "runs repo updater" do
+      repo_updater = double(:repo_updater, run: nil)
+      payload = double("Payload", github_repo_id: 1)
+      allow(Payload).to receive(:new).with(payload_data).and_return(payload)
+      allow(RepoUpdater).to receive(:new).and_return(repo_updater)
+
+      BuildableTestJob.perform_now(payload_data)
+
+      expect(Payload).to have_received(:new).with(payload_data)
+      expect(RepoUpdater).to have_received(:new).with(payload)
+      expect(repo_updater).to have_received(:run)
     end
   end
 
